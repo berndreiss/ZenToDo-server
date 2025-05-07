7faa882e581bd90975802f281e061fc9e9a4e9d1
bdreiss <bd_reiss@yahoo.de>
2025-05-07 22:23:24 +0200

    Worked on Interface

--- src/test/java/net/berndreiss/zentodo/ZenToDoServerApplicationTests.java
+++ src/test/java/net/berndreiss/zentodo/ZenToDoServerApplicationTests.java
@@ -1,13 +1,23 @@
 package net.berndreiss.zentodo;
 
+import net.berndreiss.zentodo.data.ClientOperationHandler;
+import net.berndreiss.zentodo.util.TestDbHandler;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import net.berndreiss.zentodo.util.ClientStub;
 @SpringBootTest
 class ZenToDoServerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		ZenToDoServerApplication.main(null);
+		System.out.println("HERE");
+		ClientOperationHandler opHandler = new TestDbHandler("ZenToDoPU");
+		ClientStub stub = new ClientStub("bd_reiss@yahoo.de", opHandler);
+		stub.init(()->"Test123!?");
+
 	}
+
+
 
 }
